a77e4fd74bd916fa180f2e6bbd05d12b36a0fd73
Rana Ahmad <taimur1871@gmail.com>
2021-02-16 16:38:46 -0700

    add grading functions

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import shutil, os
 from pathlib import Path
 
 UPLOAD_FOLDER = 'uploads'
+image_folder = 'static/sample_img'
 
 # 2. Create app and model objects
 app = FastAPI()
@@ -20,5 +21,25 @@ templates = Jinja2Templates(directory="templates/")
 #3. Welcome page
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request:Request):
+    img_list = os.listdir(image_folder)
     return templates.TemplateResponse("welcome.html", 
-    {"request": request, "message":"select source folder"})
+    {"request": request, "image":img_list[0], "resp":"None"})
+
+
+@app.post("/0")
+async def move_zero(request:Request):
+    '''
+    tmp_uploads_path = './uploads/0/'
+
+    if not os.path.exists(tmp_uploads_path):
+        os.makedirs(tmp_uploads_path)
+
+    p = Path(tmp_uploads_path + file.filename)
+    #shutil.move(Request.query_params, p)
+    
+    img_list = os.listdir(image_folder)
+    return templates.TemplateResponse("welcome.html", 
+    {"request": request, "image":img_list[0], "resp":response})'''
+    bod = request.body
+    
+    return {"body":bod}
