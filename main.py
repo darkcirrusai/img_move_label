065fda1ed9a0c8f076e05d286ed0056dd259ebd7
Rana Ahmad <taimur1871@gmail.com>
2021-02-17 12:21:45 -0700

    app working

--- main.py
+++ main.py
@@ -2,24 +2,14 @@
 # -*- coding: utf-8 -*-
 
 # 1. Library imports
-from typing import KeysView, NoReturn, ValuesView
-from fastapi import responses
-from starlette.responses import FileResponse
-from fastapi import FastAPI, Request, File, UploadFile
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import shutil, os
-from pathlib import Path
-from pydantic import BaseModel
 
 UPLOAD_FOLDER = 'uploads'
 image_folder = 'static/sample_img'
-
-
-class img(BaseModel):
-    name: str
-    label: int
 
 # 2. Create app and model objects
 app = FastAPI()
@@ -30,28 +20,23 @@ templates = Jinja2Templates(directory="templates/")
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request:Request):
     img_list = os.listdir(image_folder)
+    pic_rem = len(img_list)
     return templates.TemplateResponse("welcome.html", 
-    {"request": request, "image":img_list[0]})
+    {"request": request, "image":img_list[0], "pic_rem":pic_rem})
 
-@app.post("/img/{name}")
-async def create_item(img: img):
-    return img
+@app.get("/img/{item_id}")
+def move(item_id: str, request: Request):
+    val = request.query_params
+    label = val["label"]
+    pic_name = val["name"]
 
+    upload_path = "./uploads/"+label+"/"+pic_name
+    image_path = 'static/sample_img/'+pic_name
 
-'''
-@app.get("/0")
-async def move_zero(request:Request,
-                    ):
-    
-    tmp_uploads_path = './uploads/0/'
+    pic_rem = len(os.listdir('static/sample_img'))
 
-    if not os.path.exists(tmp_uploads_path):
-        os.makedirs(tmp_uploads_path)
-
-    p = Path(tmp_uploads_path + file.filename)
-    #shutil.move(Request.query_params, p)
+    shutil.move(image_path, upload_path)
     
     img_list = os.listdir(image_folder)
     return templates.TemplateResponse("welcome.html", 
-    {"request": request, "image":img_list[0]})
-    '''
+    {"request": request, "image":img_list[0], "pic_rem":pic_rem})
