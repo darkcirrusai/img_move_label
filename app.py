795e8119eeaf6474179ec903e7877d8ed13e2412
taimur1871 <taimur1871@gmail.com>
2022-11-19 13:44:00 -0700

    Removed images and clean code

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ app = FastAPI()
 # 1. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='127.0.0.1', port=8000, reload = True)
+    uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 # 1. Library imports
 from fastapi import FastAPI, Request
@@ -8,21 +6,23 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import shutil, os
 
-UPLOAD_FOLDER = 'uploads'
-image_folder = 'static/cat_1'
+UPLOAD_FOLDER = 'sorted_files'
+image_folder = 'source_files'
 
 # 2. Create app and model objects
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates/")
 
-#3. Welcome page
+
+# 3. Welcome page
 @app.get("/", response_class=HTMLResponse)
-async def read_root(request:Request):
+async def read_root(request: Request):
     img_list = os.listdir(image_folder)
     pic_rem = len(img_list)
-    return templates.TemplateResponse("welcome.html", 
-    {"request": request, "image":img_list[0], "pic_rem":pic_rem})
+    return templates.TemplateResponse("welcome.html",
+                                      {"request": request, "image": img_list[0], "pic_rem": pic_rem})
+
 
 @app.get("/img/{item_id}")
 def move(item_id: str, request: Request):
@@ -30,20 +30,20 @@ def move(item_id: str, request: Request):
     label = val["label"]
     pic_name = val["name"]
 
-    upload_path = "./uploads/"+label+"/"+pic_name
-    image_path = 'static/cat_1/'+pic_name
+    upload_path = "./sorted_files/" + label + "/" + pic_name
+    image_path = 'static/cat_1/' + pic_name
 
     # folders for individual grade
-    pic4 = len(os.listdir('./uploads/0'))
-    pic5 = len(os.listdir('./uploads/1'))
-    pic6 = len(os.listdir('./uploads/2'))
-    pic7 = len(os.listdir('./uploads/3'))
+    pic4 = len(os.listdir('sorted_files/0'))
+    pic5 = len(os.listdir('sorted_files/1'))
+    pic6 = len(os.listdir('sorted_files/2'))
+    pic7 = len(os.listdir('sorted_files/3'))
 
     pic_rem = len(os.listdir('static/cat_1'))
 
     shutil.move(image_path, upload_path)
-    
+
     img_list = os.listdir(image_folder)
-    return templates.TemplateResponse("welcome.html", 
-    {"request": request, "image":img_list[0], "pic_rem":pic_rem,
-    "pic4":pic4, "pic5":pic5, "pic6":pic6, "pic7":pic7,})
+    return templates.TemplateResponse("welcome.html",
+                                      {"request": request, "image": img_list[0], "pic_rem": pic_rem,
+                                       "pic4": pic4, "pic5": pic5, "pic6": pic6, "pic7": pic7, })
